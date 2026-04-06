7255cd1e0b1c9195ab0705b41703bc91589695e0
Lele <Khoi.lenguyen3010@hcmut.edu.vn>
2026-04-06 22:15:03 +0700

    Fix Web

--- Sport-Dashboard/next.config.ts
+++ Sport-Dashboard/next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Thêm block này vào
+const nextConfig: NextConfig = {
   experimental: {
-    allowedDevOrigins: [
-      "172.28.182.82", // IP của thiết bị bạn đang dùng để test
-      "localhost:3000", // Giữ lại localhost để máy tính vẫn truy cập bình thường
-    ],
+    serverActions: {
+      allowedOrigins: [
+        "172.28.182.82", 
+        "localhost:3000",
+        "sport-dashboard-three.vercel.app" 
+      ],
+    },
   },
 };
 
